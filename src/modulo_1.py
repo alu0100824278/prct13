a646657b2413cee34ff68e30257382ace8d8d2d1
JORGE ANTONIO <alu0100824278@administrador-OptiPlex-755.(none)>
2014-05-09 10:19:53 +0100

    prct13

---
+++ src/modulo_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#! encoding: UTF-8
+
+#Utilizamos el mismo modulo1 que en la prct12
+
+import sys
+
+PI=3.1415926535897931159979634685441852
+
+def aprox(fin):
+  if fin > 0:
+    suma = 0.0
+    for i in range(1, fin+1):
+      a = (i-1.0)/fin
+      b = (i+0.0)/fin
+      xi = (i-0.5)/fin
+      fxi = 4.0/(1.0+xi*xi)
+      suma = suma + fxi
+    aprox = suma/fin
+  return aprox 
+  
+if __name__=="__main__":
+   if (len(sys.argv)==3):
+     p1 = int(sys.argv[1])
+     p2 = int(sys.argv[2])
+   else:
+     print "El programa se ejecutará por defecto con los valores predeterminados 10 y 10"
+     p1 = 10
+     p2 = 10
+   l = []
+   for i in range(1,p2+1):
+     p1 = p1*i
+     s = aproxpi(p1)
+     print "El valor aproximado de PI es: %11.10f" % s
+     l = l + [s]
+   print l 
